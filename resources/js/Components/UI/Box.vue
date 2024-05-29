b2e9b90c5c56f02d635d8a97df76afaf93966e27
Reepal Bhavsar <reepal231994@gmail.com>
2024-05-29 16:42:18 +0200

    added responsive style in show listing details page

--- resources/js/Components/UI/Box.vue
+++ resources/js/Components/UI/Box.vue
@@ -1,3 +1,8 @@
 <template>
-    <div class="border border-gray-200 dark:border-gray-800 rounded-md p-4 shadow-sm dark:text-gray-300"><slot /></div>
+    <div class="border border-gray-200 dark:border-gray-800 rounded-md p-4 shadow-sm dark:text-gray-300">
+        <div class="font-medium text-gray-500">
+            <slot name="header" />
+        </div>
+        <slot />
+    </div>
 </template>
