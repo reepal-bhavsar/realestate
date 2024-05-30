a3e3d2a7f4d0c45faec08b5134c7f0f0aefabae2
Reepal Bhavsar <reepal231994@gmail.com>
2024-05-30 16:43:55 +0200

    Created a common error message component

---
+++ resources/js/Components/UI/ErrorMessage.vue
@@ -0,0 +1,9 @@
+<template>
+    <span class="input-error">{{ errorMsg }}</span>
+</template>
+
+<script setup>
+const props = defineProps({
+    errorMsg: String,
+})
+</script>
--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,42 +4,43 @@
             <div class="col-span-2">
                 <label class="label">Beds</label>
                 <input class="input" v-model.number="form.beds" type="text" />
-                <div class="input-error" v-if="form.errors.beds">{{ form.errors.beds}}</div>
+                <ErrorMessage v-if="form.errors.beds" :errorMsg="form.errors.beds" />
             </div>
             <div class="col-span-2">
                 <label class="label">Baths</label>
                 <input class="input" v-model.number="form.baths" type="text" />
-                <div class="input-error" v-if="form.errors.baths">{{ form.errors.baths }}</div>
+                <ErrorMessage v-if="form.errors.baths" :errorMsg="form.errors.baths" />
+                
             </div>
             <div class="col-span-2">
                 <label class="label">Area</label>
                 <input class="input" v-model.number="form.area" type="text" />
-                <div class="input-error" v-if="form.errors.area">{{ form.errors.area }}</div>
+                <ErrorMessage v-if="form.errors.area" :errorMsg="form.errors.area" />
             </div>
             <div class="col-span-4">
                 <label class="label">City</label>
                 <input class="input" v-model="form.city" type="text" />
-                <div class="input-error" v-if="form.errors.area">{{ form.errors.area }}</div>
+                <ErrorMessage v-if="form.errors.city" :errorMsg="form.errors.city" />
             </div>
             <div class="col-span-2">
                 <label class="label">Post Code</label>
                 <input class="input" v-model="form.code" type="text" />
-                <div class="input-error" v-if="form.errors.code">{{ form.errors.code }}</div>
+                <ErrorMessage v-if="form.errors.code" :errorMsg="form.errors.code" />
             </div>
             <div class="col-span-4">
                 <label class="label">Street</label>
                 <input class="input" v-model="form.street" type="text" />
-                <div class="input-error" v-if="form.errors.street">{{ form.errors.street }}</div>
+                <ErrorMessage v-if="form.errors.street" :errorMsg="form.errors.street" />
             </div>
             <div class="col-span-2">
                 <label class="label">Street Nr</label>
                 <input class="input" v-model.number="form.street_nr" type="text" />
-                <div class="input-error" v-if="form.errors.street_nr">{{ form.errors.street_nr }}</div>
+                <ErrorMessage v-if="form.errors.street_nr" :errorMsg="form.errors.street_nr" />
             </div>
             <div class="col-span-6">
                 <label class="label">Price</label>
                 <input class="input" v-model.number="form.price" type="text" />
-                <div class="input-error" v-if="form.errors.price">{{ form.errors.price }}</div>
+                <ErrorMessage v-if="form.errors.price" :errorMsg="form.errors.price" />
             </div>
             <div class="col-span-6">
                 <button class="btn-primary" type="submit">Create</button>
@@ -49,6 +50,7 @@
 </template>
 
 <script setup>
+    import ErrorMessage from '@/Components/UI/ErrorMessage.vue';
     import { useForm } from '@inertiajs/vue3'
 
     const form = useForm({
