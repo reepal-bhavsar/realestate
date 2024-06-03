c909a88e3b36d0663699ce91867f72abd63ff55a
Reepal Bhavsar <reepal231994@gmail.com>
2024-06-03 17:55:27 +0200

    Authentication Controller and Login form creation

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <form @submit.prevent="login">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label for="email" class="label">E-mail</label>
+                <input v-model="form.email" id="email" type="text" class="input" />
+                <div class="input-error" v-if="form.errors.email">{{ form.errors.email }}</div>
+            </div>
+            <div class="mt-4">
+                <label for="password" class="label">Password</label>
+                <input v-model="form.password" id="password" type="password" class="input" />
+                <div class="input-error" v-if="form.errors.password">{{ form.errors.password }}</div>
+            </div>
+            <div class="mt-4">
+                <button type="submit" class="btn-primary w-full">Login</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    email: null,
+    password: null
+})
+
+const login = () => form.post(route('login.store'))
+</script>
